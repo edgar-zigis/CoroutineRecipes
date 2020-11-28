30b3f22c2d257fc773b4ec26d8c017ada985a961
Edgar Žigis <tranced.freak@gmail.com>
2020-11-28 12:25:15 +0200

    Add additional testing cases for BasicFlow

--- src/main/kotlin/Flows.kt
+++ src/main/kotlin/Flows.kt
@@ -1,13 +1,23 @@
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.runBlocking
 
 fun main() {
     testBasicFlowOperators()
+    //testBasicFlowCollect()
+    //testBasicFlowDistinctCollection()
+    //testBasicFlowDebounceCollection()
 }
+
+//  ****
+//  Basic Flow operators
+//  ****
 
 fun testBasicFlowOperators() = runBlocking {
     basicFlow().onEach {
         println("BasicFlow emitted value: $it")
+    }.onStart {
+        println("BasicFlow has started its work")
     }.onCompletion {
         println("BasicFlow has finished its work")
     }.catch {
@@ -15,8 +25,27 @@ fun testBasicFlowOperators() = runBlocking {
     }.launchIn(this)
 }
 
+fun testBasicFlowCollect() = runBlocking {
+    basicFlow().collect {
+        println("BasicFlowCollect emitted value: $it")
+    }
+}
+
+fun testBasicFlowDistinctCollection() = runBlocking {
+    basicFlow().distinctUntilChanged().collect {
+        println("BasicFlowDistinctCollect emitted value: $it")
+    }
+}
+
+fun testBasicFlowDebounceCollection() = runBlocking {
+    basicFlow().debounce(100).collect {
+        println("BasicFlowDebounceCollect emitted value: $it")
+    }
+}
+
 fun basicFlow() : Flow<Int> = flow {
-    for (i in 0 until 5) {
+    for (i in 0 until 3) {
+        emit(i)
         emit(i)
         if (i == 3) throw Error()
     }
