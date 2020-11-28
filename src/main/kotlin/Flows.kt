135ee740ba4526d98dd4e4c9de3c22572d88ba19
Edgar Žigis <tranced.freak@gmail.com>
2020-11-28 15:52:18 +0200

    Add explanations

--- src/main/kotlin/Flows.kt
+++ src/main/kotlin/Flows.kt
@@ -20,8 +20,29 @@ fun main() {
 //  Basic Flow operators
 //  ****
 
+fun basicFlow(): Flow<Int> = flow {
+    for (i in 0 until 3) {
+        emit(i)
+        delay(100)
+        emit(i)
+    }
+}.flowOn(Dispatchers.IO)
+
+fun basicFlowWithException(): Flow<Int> = flow {
+    for (i in 0 until 3) {
+        emit(i)
+        delay(100)
+        emit(i)
+        if (i == 1) throw Error()
+    }
+}.flowOn(Dispatchers.IO)
+
+/*
+    This will output 'started', 0 0 1 1 'finished' 'exception'
+    if catch is not used -> process will crash and exception will be passed upstream
+ */
 fun testBasicFlowOperators() = runBlocking {
-    basicFlow().onEach {
+    basicFlowWithException().onEach {
         println("BasicFlow emitted value: $it")
     }.onStart {
         println("BasicFlow has started its work")
@@ -32,30 +53,50 @@ fun testBasicFlowOperators() = runBlocking {
     }.launchIn(this)
 }
 
+/*
+    This will output 0 0 1 1 and then process will crash
+    Exception is passed upstream
+ */
 fun testBasicFlowCollect() = runBlocking {
-    basicFlow().collect {
+    basicFlowWithException().collect {
         println("BasicFlowCollect emitted value: $it")
     }
 }
 
+/*
+    This will output 0 1 2
+    distinctUntilChanged ignores any duplicates
+ */
 fun testBasicFlowDistinctCollection() = runBlocking {
     basicFlow().distinctUntilChanged().collect {
         println("BasicFlowDistinctCollect emitted value: $it")
     }
 }
 
+/*
+    This will output 0 1 2 2
+    debounce is ignoring any values emitted faster than the previous one
+ */
 fun testBasicFlowDebounceCollection() = runBlocking {
-    basicFlow().debounce(100).collect {
+    basicFlow().debounce(80).collect {
         println("BasicFlowDebounceCollect emitted value: $it")
     }
 }
 
+/*
+    This will output 0 0 1
+    take will just take first 3 elements from the flow and finish
+ */
 fun testBasicFlowTakeCollection() = runBlocking {
     basicFlow().take(3).collect {
         println("BasicFlowDebounceCollect emitted value: $it")
     }
 }
 
+/*
+    This will output 0 0 1 1 2 2 sum 12
+    This is just a simple operator display
+ */
 fun testBasicFlowGargantuanCollection() = runBlocking {
     val someWeirdSum = basicFlow().onEach {
         delay(200)
@@ -70,21 +111,23 @@ fun testBasicFlowGargantuanCollection() = runBlocking {
     println("SomeGargantuanSum: $someWeirdSum")
 }
 
-fun basicFlow(): Flow<Int> = flow {
-    for (i in 0 until 3) {
-        emit(i)
-        emit(i)
-        if (i == 3) throw Error()
-    }
-}.flowOn(Dispatchers.IO)
-
 //  ****
 //  Shared/State Flow
 //  ****
 
+/*
+    replay represents the number of values emitted to new subscribers
+ */
 val sharedFlow = MutableSharedFlow<Int>(replay = 1)
 val stateFlow = MutableStateFlow(69)
 
+/*
+    This will output 0 0 1 1 2 2
+    Basically this acts like a channel and outputs everything
+    Try removing delays though and it will output less values,
+    because subscriber thread is too slow to listen to changes.
+    SharedFlow is a hot replacement for BroadcastChannel
+ */
 fun testSharedFlow() = runBlocking {
     sharedFlow.asSharedFlow().onEach {
         println("SharedFlow value: $it")
@@ -98,6 +141,12 @@ fun testSharedFlow() = runBlocking {
     }
 }
 
+/*
+    This will output 0 1 2
+    StateFlow is used to specifically work with states as it will only emit new states.
+    StateFlow == SharedFlow with distinctUntilChanged applied.
+    This is a perfect replacement for LiveData
+ */
 fun testStateFlow() = runBlocking {
     stateFlow.asStateFlow().onEach {
         println("StateFlow value: $it")
@@ -115,42 +164,70 @@ fun testStateFlow() = runBlocking {
 //  Callback/Channel Flow
 //  ****
 
-fun testDummyFlowWithConcurrentEmissions() = runBlocking {
-    dummyFlow().collect {
-        println("DummyFlow value: $it")
-    }
-}
-
-fun testCallbackFlowWithConcurrentEmissions() = runBlocking {
-    callbackFlow().collect {
-        println("CallbackFlow value: $it")
-    }
-}
-
-fun testChannelFlowWithConcurrentEmissions() = runBlocking {
-    channelFlow().collect {
-        println("ChannelFlow value: $it")
-    }
-}
-
+/*
+    The following methods represent an attempt to provide some callback values from different threads
+    1. Using generic Flow
+    2. CallbackFlow
+    3. ChannelFlow
+ */
 fun dummyFlow(): Flow<Int> = flow {
     GlobalScope.launch {
         emit(1)
+        emit(2)
     }
+    delay(100)
 }
 
 fun callbackFlow(): Flow<Int> = callbackFlow {
     GlobalScope.launch {
         offer(1)
+        offer(2)
         close()
     }
+    delay(100)
     awaitClose { cancel() }
 }
 
 fun channelFlow(): Flow<Int> = channelFlow {
     GlobalScope.launch {
         offer(1)
+        offer(2)
         close()
     }
+    delay(100)
     awaitClose { cancel() }
+}
+
+/*
+    This will crash. Because emission from any other or coroutine is not allowed.
+    This can't be used for callbacks.
+ */
+fun testDummyFlowWithConcurrentEmissions() = runBlocking {
+    dummyFlow().collect {
+        println("DummyFlow value: $it")
+    }
+}
+
+/*
+    This will work fine and emit 1 2. CallbackFlow is designed to deal specifically with callbacks.
+    Great replacement for custom Rx Callback Observables
+ */
+fun testCallbackFlowWithConcurrentEmissions() = runBlocking {
+    callbackFlow().collect {
+        println("CallbackFlow value: $it")
+    }
+}
+
+/*
+    This will work fine and emit 1 2, same like callbackFlow
+    What's the difference? Try removing awaitClose from callbackFlow and it will crash
+    channelFlow - won't. Basically the difference is very minor, callbackFlow is more secure
+    awaitClose should be used to close any resources (like observers).
+    Also, do not forget to call close() which notifies flow that there is nothing to listen to.
+    close() should be called when the final action was performed.
+ */
+fun testChannelFlowWithConcurrentEmissions() = runBlocking {
+    channelFlow().collect {
+        println("ChannelFlow value: $it")
+    }
 }
